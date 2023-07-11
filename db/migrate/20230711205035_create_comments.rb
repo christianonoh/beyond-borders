79de60b6e0675999a10be62dc23ebe4212e6ba9e
christianonoh <onohchibyk@gmail.com>
2023-07-11 23:13:17 +0100

    Add reference to user for author

--- db/migrate/20230711205035_create_comments.rb
+++ db/migrate/20230711205035_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.references :author, null: false, foreign_key: true
+      t.references :author, null: false, foreign_key: { to_table: :users }
       t.belongs_to :post, null: false, foreign_key: true
       t.text :text
 
