6c8a52dd14b4a013958f6471154769857ac3494f
christianonoh <onohchibyk@gmail.com>
2023-07-13 03:09:32 +0100

    Add methods to increment and decrement post count

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,18 @@
 class Post < ApplicationRecord
-  belongs_to :users, foreign_key: 'author_id'
+  belongs_to :user, foreign_key: 'author_id'
   has_many :comments
   has_many :likes
+
+  after_create :increment_post_count
+  after_destroy :decrement_post_count
+
+  def increment_post_count
+    puts 'Incrementing post count'
+    user.increment!(:posts_counter)
+  end
+
+  def decrement_post_count
+    puts 'Decrementing post count'
+    user.decrement!(:posts_counter)
+  end
 end
