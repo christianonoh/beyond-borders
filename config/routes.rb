284fad2bc2b5897aab7ac5a06565d464d1b6ec5e
christianonoh <onohchibyk@gmail.com>
2023-07-18 13:38:46 +0100

    Modify routes.rb to fix routing error

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # root "articles#index"
   get '/users', to: 'users#index', as: 'users'
   get '/users/:id', to: 'users#show', as: 'user'
-  get '/users/:id/posts', to: 'posts#index', as: 'user_posts'
-  get '/users/:id/posts/:post_id', to: 'posts#show'
+  get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
+  get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
+  # get '/users/:id/posts', to: 'posts#index', as: 'user_posts'
+  # get '/users/:id/posts/:post_id', to: 'posts#show'
 end
