7f236e2827ce60341bc538ffc62891ef249a0c87
christianonoh <onohchibyk@gmail.com>
2023-07-21 17:49:52 +0100

    Modify routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:new, :create]
+      # resource :likes, only: [:create, :destroy]
+    end
   end
 end
