07ca31e65f3d256ded145c281136822cd0eb3dda
christianonoh <onohchibyk@gmail.com>
2023-07-25 06:47:19 +0100

    Create post_index_spec.rb file

---
+++ spec/integration/post_index_spec.rb
@@ -0,0 +1,58 @@
+require 'rails_helper'
+
+RSpec.describe 'User posts', type: :feature do
+  describe 'Show Page' do
+    before(:each) do
+      @user1 = User.create(name: 'Usom', photo: 'https://source.unsplash.com/user/c_v_r/100x100', bio: 'Usom bio')
+      @user2 = User.create(name: 'Burger', photo: 'https://i.picsum.photos/id/216/200/300.jpg?hmac', bio: 'Burger bio')
+      @post1 = Post.create(title: 'Post 1', text: 'Post content goes here', author_id: @user1.id)
+      @comment1 = Comment.create(post_id: @post1.id, text: 'First comment goes here', author_id: @user1.id)
+      @comment2 = Comment.create(post_id: @post1.id, text: 'Second comment goes here', author_id: @user2.id)
+      @comment3 = Comment.create(post_id: @post1.id, text: 'Third comment goes here', author_id: @user2.id)
+      @like1 = Like.create(author_id: @user1.id, post_id: @post1.id)
+      @like2 = Like.create(author_id: @user1.id, post_id: @post1.id)
+      @like3 = Like.create(author_id: @user2.id, post_id: @post1.id)
+
+      visit user_posts_path(@user1)
+    end
+
+    it 'should have profile picture content of all other users' do
+      expect(page).to have_css("img[src*='/images/default.jpg']")
+    end
+
+    it 'should have user name' do
+      expect(page).to have_content(@user1.name)
+    end
+
+    it 'should have user bio' do
+      expect(page).to have_content(@user1.bio)
+    end
+
+    it 'should have part content of the post body' do
+      expect(page).to have_content(@post1.text[0, 50])
+    end
+
+    it 'displays the number of comments each post has' do
+      expect(page).to have_content("Comments: #{@post1.comments.count}")
+    end
+
+    it 'displays the number of likes each post has' do
+      expect(page).to have_content("Comments: #{@post1.likes.count}")
+    end
+
+    it 'should see post body' do
+      expect(page).to have_content(@post1.text)
+    end
+
+    it 'should have post first 3 comments' do
+      expect(page).to have_content(@comment1.text)
+      expect(page).to have_content(@comment2.text)
+      expect(page).to have_content(@comment3.text)
+    end
+
+    it 'should redirect to the post page when a post is clicked' do
+      click_link 'Post 1'
+      expect(page).to have_current_path(user_post_path(@user1, @post1))
+    end
+  end
+end
