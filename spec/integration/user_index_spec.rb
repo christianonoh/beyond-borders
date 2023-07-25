69b83c6a7626c1f4d2b2c6382914b00640ddf94c
christianonoh <onohchibyk@gmail.com>
2023-07-25 12:34:06 +0100

    Test the profile picture with a different src value.

--- spec/integration/user_index_spec.rb
+++ spec/integration/user_index_spec.rb
@@ -2,20 +2,20 @@ require 'rails_helper'
 
 RSpec.describe 'User index page', type: :feature do
   before(:each) do
-    @user1 = User.create(name: 'Usom Factory', photo: 'https://source.unsplash.com/user/c_v_r/100x100', bio: 'Usom bio')
+    @user1 = User.create(name: 'Usom Factory', photo: 'https://dummyimage.com/200x200/3498db/ffffff', bio: 'Usom bio')
     @user2 = User.create(name: 'Burger', photo: 'https://i.picsum.photos/id/216/200/300.jpg?hmac', bio: 'Burger bio')
 
     visit users_path
   end
 
   it 'displays the username of all other users' do
-    expect(page).to have_content('Kotonko')
-    expect(page).to have_content('Usom')
+    expect(page).to have_content('Usom Factory')
+    expect(page).to have_content('Burger')
   end
 
   it 'should have profile picture content of all other users' do
-    expect(page).to have_css("img[src*='/images/default.jpg']")
-    expect(page).to have_css("img[src*='/images/default.jpg']")
+    expect(page).to have_css("img[src*='https://dummyimage.com/200x200/3498db/ffffff']", visible: :visible)
+    expect(page).to have_css("img[src*='https://i.picsum.photos/id/216/200/300.jpg?hmac']", visible: :visible)
   end
 
   it 'displays the number of posts each user has written' do
