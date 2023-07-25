3c62e62a8dae95e7a6db20253b59dbdbfd55b5af
christianonoh <onohchibyk@gmail.com>
2023-07-25 13:45:51 +0100

    Test the profile picture with a src value.

--- spec/integration/post_index_spec.rb
+++ spec/integration/post_index_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe 'User posts', type: :feature do
   describe 'Show Page' do
     before(:each) do
-      @user1 = User.create(name: 'Usom', photo: 'https://source.unsplash.com/user/c_v_r/100x100', bio: 'Usom bio')
-      @user2 = User.create(name: 'Burger', photo: 'https://i.picsum.photos/id/216/200/300.jpg?hmac', bio: 'Burger bio')
+      @user1 = User.create(name: 'Usom', photo: 'https://dummyimage.com/200x200/3498db/ffffff', bio: 'Usom bio')
+      @user2 = User.create(name: 'Burger', photo: 'https://images.unsplash.com/photo-1661956600684-97d3a4320e45?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80', bio: 'Burger bio')
       @post1 = Post.create(title: 'Post 1', text: 'Post content goes here', author_id: @user1.id)
       @comment1 = Comment.create(post_id: @post1.id, text: 'First comment goes here', author_id: @user1.id)
       @comment2 = Comment.create(post_id: @post1.id, text: 'Second comment goes here', author_id: @user2.id)
@@ -16,8 +16,8 @@ RSpec.describe 'User posts', type: :feature do
       visit user_posts_path(@user1)
     end
 
-    it 'Should have profile picture content of all other users' do
-      expect(page).to have_css("img[src*='/images/default.jpg']")
+    it 'Should have profile picture content of user' do
+      expect(page).to have_css("img[src*='https://dummyimage.com/200x200/3498db/ffffff']", visible: :visible)
     end
 
     it 'should have user name' do
