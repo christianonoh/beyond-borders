2a6886283394013e5164d12e3ef8b395048c354f
christianonoh <onohchibyk@gmail.com>
2023-07-27 10:22:21 +0100

    Modify development.rb to use letter_opener

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,12 +2,14 @@ require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :letter_opener
+  
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-
+  
   # Do not eager load code on boot.
   config.eager_load = false
 
@@ -67,6 +69,4 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
