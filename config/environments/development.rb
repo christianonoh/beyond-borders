23cdfe3e66e956556a3995a73f65275b8d228d05
Vitoesi Elijah <70116262+Ellyboi@users.noreply.github.com>
2023-07-27 11:26:06 +0100

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,13 +41,13 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.perform_caching = false
-
-  # Print deprecation notices to the Rails logger.
+   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
   # Raise exceptions for disallowed deprecations.
   config.active_support.disallowed_deprecation = :raise
+
+  config.action_mailer.perform_caching = false
 
   # Tell Active Support which deprecation messages to disallow.
   config.active_support.disallowed_deprecation_warnings = []
