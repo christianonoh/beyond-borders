31ac86555753f1a1dea8120d52645a66c5b7d3d3
christianonoh <onohchibyk@gmail.com>
2023-07-29 16:11:21 +0100

    fix linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,12 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  
+
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
-  
-    before_save :generate_api_token
+
+  before_save :generate_api_token
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
