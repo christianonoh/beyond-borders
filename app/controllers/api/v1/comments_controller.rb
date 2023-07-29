dc74d2a5183181cb9ba681850bc4a98dca29241f
christianonoh <onohchibyk@gmail.com>
2023-07-29 14:07:30 +0100

    Add API comments controller

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,2 +1,26 @@
 class Api::V1::CommentsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:create], if: -> { request.format.json? }
+
+  def index
+    user = User.find(params['user_id'])
+    post = user.posts.find(params['post_id'])
+    comments = post.comments
+    render json: comments
+  end
+
+  def create
+    token = request.headers['X-Token']
+    user = User.find_by(api_token: token)
+    post = Post.find(params['post_id'])
+
+    new_comment = post.comments.new(
+      text: params['text'],
+      user:
+    )
+
+    new_comment.save
+    render json: { success: 'Comment added successfully' }
+  rescue StandardError => e
+    render json: { error: e.message }
+  end
 end
